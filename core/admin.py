c134a8eb7259ed26830e2c1fb84ad8764c1d961d
davisantosr <davi-rocha2@hotmail.com>
2021-02-11 16:46:25 -0300

    models|admin|templates

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Role, Services, Team
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+  list_display = ('role', 'active', 'updated')
+
+@admin.register(Services)
+class ServiceAdmin(admin.ModelAdmin):
+  list_display = ('service', 'icon', 'active', 'updated')
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+  list_display = ('name', 'role')
